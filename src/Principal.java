5c1274930594c350cf2eff2e20b5a78f72c6e26d
Drezsa <emailandrezadasilva@gmail.com>
2025-11-20 15:33:19 -0300

    concertando o if

--- src/Principal.java
+++ src/Principal.java
@@ -10,32 +10,47 @@ public class Principal {
         System.out.println("SEJA BEM VINDO AO CONVERSOR DE MOEDAS\n");
 
         int opcao = 0;
-        while (opcao != 7) {
-            System.out.println("1- Dolar =>> Peso Argentino");
-            System.out.println("2- Peso Argentino =>> Dolar");
-            System.out.println("3- Dolar =>> Real Brasileiro");
-            System.out.println("4- Real Brasileiro =>> Dolar");
-            System.out.println("5- Dolar =>> Peso Colombiano");
-            System.out.println("6- Peso Colombiano =>> Dolar");
-            System.out.println("7- Sair");
-            System.out.println("Escolha uma opção válida: ");
-            opcao = leitura.nextInt();
-            System.out.println("*************************************************************\n");
+        try {
+            while (opcao != 7) {
+                System.out.println("1- Dolar =>> Peso Argentino");
+                System.out.println("2- Peso Argentino =>> Dolar");
+                System.out.println("3- Dolar =>> Real Brasileiro");
+                System.out.println("4- Real Brasileiro =>> Dolar");
+                System.out.println("5- Dolar =>> Peso Colombiano");
+                System.out.println("6- Peso Colombiano =>> Dolar");
+                System.out.println("7- Sair");
+                System.out.println("Escolha uma opção válida: ");
+                opcao = leitura.nextInt();
+                System.out.println("*************************************************************\n");
 
-            if (opcao == 1){
-                try{
-                    Moedas novaMoeda = converteMoeda.buscaMoedas(opcao);
-                    System.out.println(novaMoeda);
-                }catch (RuntimeException | IOException e){
-                    System.out.println(e.getMessage());
-                    System.out.println("Finalizando aplicação");
+                if (opcao == 1) {
+                    Moedas novaMoeda = converteMoeda.buscaMoedas("USD", "ARS");
+                    System.out.println("1 " + novaMoeda.origem() + " = " + novaMoeda.taxa() + " " + novaMoeda.destino());
+                } else if (opcao == 2) {
+                    Moedas novaMoeda = converteMoeda.buscaMoedas("ARS", "USD");
+                    System.out.println("1" + novaMoeda.origem() + " = " + novaMoeda.taxa() + " " + novaMoeda.destino());
+                } else if (opcao == 3){
+                    Moedas novaMoeda = converteMoeda.buscaMoedas("USD", "BRL");
+                    System.out.println("1" + novaMoeda.origem() + " = " + novaMoeda.taxa() + " " + novaMoeda.destino());
+                } else if (opcao == 4) {
+                    Moedas novaMoeda = converteMoeda.buscaMoedas("BRL", "USD");
+                    System.out.println("1" + novaMoeda.origem() + " = " + novaMoeda.taxa() + " " + novaMoeda.destino());
+                } else if (opcao == 5) {
+                    Moedas novaMoeda = converteMoeda.buscaMoedas("USD", "COP");
+                    System.out.println("1" + novaMoeda.origem() + " = " + novaMoeda.taxa() + " " + novaMoeda.destino());
+                } else if (opcao == 6) {
+                    Moedas novaMoeda = converteMoeda.buscaMoedas("COP", "USD");
+                    System.out.println("1" + novaMoeda.origem() + " = " + novaMoeda.taxa() + " " + novaMoeda.destino());
+                } else if (opcao == 7) {
+                    System.out.println("Saindo...");
+                } else {
+                    System.out.println("Opção inválida!");
                 }
-            } else if (opcao == 2) {
-                System.out.println("bom");
-            }else {
-                System.out.println("Saindo...");
             }
 
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando aplicação");
         }
     }
 }
